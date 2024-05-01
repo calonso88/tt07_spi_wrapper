85b05c4f52cfef22a6a5ef9d0cca6619113d0f54
Caio Alonso da Costa <caioalonso@gmail.com>
2024-05-01 22:11:16 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_calonso88_spi_trial (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,9 +16,35 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  // SPI Auxiliars
+  wire spi_cs_n;
+  wire spi_clk;
+  wire spi_miso;
+  wire spi_mosi;
+
+  // Regs Width
+  localparam int NUM_REGS = 8;
+  localparam int REG_WIDTH = 8;
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out[2:0]  = '0;
+  assign uo_out[7:5]  = '0;
+
+  // Output ports
+  assign uo_out[3] = spi_miso;
+  assign uo_out[4] = '0;
+
+  // Assign IOs as output
+  assign uio_oe       = '1;
+  // Assign spare to output
+  assign uio_out[7:0] = '0;
+
+  // Input ports
+  assign spi_cs_n   = ui_in[0];
+  assign spi_clk    = ui_in[1];
+  assign spi_mosi   = ui_in[2]; 
+
+  // SPI wrapper
+  //spi_wrapper #(.NUM_REGS(NUM_REGS), .WIDTH(REG_WIDTH)) spi_wrapper_i (.rstb(rst_n), .clk(clk), .ena(ena), .spi_cs_n(spi_cs_n), .spi_clk(spi_clk), .spi_mosi(spi_mosi), .spi_miso(spi_miso), .rw_reg('0), .stat_reg('0));
 
 endmodule
