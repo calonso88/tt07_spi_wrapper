ad12f28775cb80748a883f86b7f8887bec51bccb
Caio Alonso da Costa <caioalonso@gmail.com>
2025-04-27 20:25:30 +0200

    Rever to 8 bytes.

--- src/tt_um_calonso88_spi_trial.v
+++ src/tt_um_calonso88_spi_trial.v
@@ -60,7 +60,7 @@ module tt_um_calonso88_spi_trial (
 
   // Amount of CFG Regs and Status Regs + Regs Width
   // Limitation: NUM_CFG must be equal to NUM_STATUS
-  localparam int NUM_CFG = 4;
+  localparam int NUM_CFG = 8;
   localparam int NUM_STATUS = NUM_CFG;
   localparam int REG_WIDTH = 8;
   
@@ -76,11 +76,11 @@ module tt_um_calonso88_spi_trial (
   assign status_regs[15:8]  = 8'h10;
   assign status_regs[23:16] = 8'hAA;
   assign status_regs[31:24] = 8'h55;
-  //assign status_regs[39:32] = 8'hFF;
+  assign status_regs[39:32] = 8'hFF;
   ////assign status_regs[39:32] = {8{reduction}};
-  //assign status_regs[47:40] = 8'h00;
-  //assign status_regs[55:48] = 8'hA5;
-  //assign status_regs[63:56] = 8'h5A;
+  assign status_regs[47:40] = 8'h00;
+  assign status_regs[55:48] = 8'hA5;
+  assign status_regs[63:56] = 8'h5A;
 /*
   assign status_regs[127:64] = '1;
 */
